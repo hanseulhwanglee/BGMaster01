19b612c2a39dc176075480749c8f422e61a90a51
kyu <regularity0328@gmail.com>
2022-08-16 14:48:12 +0900

    #220816 규 1. boardDTO writer->userid getter setter 다시 수정

--- src/main/java/bgm/bgmaster/domain/BoardDTO.java
+++ src/main/java/bgm/bgmaster/domain/BoardDTO.java
@@ -30,63 +30,63 @@ public class BoardDTO {
 	public int getBno() {
 		return bno;
 	}
-	
+
 	public void setBno(int bno) {
 		this.bno = bno;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
-	
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
-	public String getWriter() {
+
+	public String getUserid() {
 		return userid;
 	}
-	
-	public void setWriter(String writer) {
-		this.userid = writer;
+
+	public void setUserid(String userid) {
+		this.userid = userid;
 	}
-	
+
 	public String getReg_date() {
 		return reg_date;
 	}
-	
+
 	public void setReg_date(String reg_date) {
 		this.reg_date = reg_date;
 	}
-	
+
 	public int getView_cnt() {
 		return view_cnt;
 	}
-	
+
 	public void setView_cnt(int view_cnt) {
 		this.view_cnt = view_cnt;
 	}
-	
+
 	public String getFile_name() {
 		return file_name;
 	}
-	
+
 	public void setFile_name(String file_name) {
 		this.file_name = file_name;
 	}
-	
+
 	public int getFile_size() {
 		return file_size;
 	}
-	
+
 	public void setFile_size(int file_size) {
 		this.file_size = file_size;
 	}
